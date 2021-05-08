08d345a8d36f35cc7bea02a9522b5d128f95d11d
deng guibao <guibao.deng@furongcloud.com>
2021-05-08 16:29:46 +0800

    fix pip freeze

--- all_in_one.sh
+++ all_in_one.sh
@@ -3,7 +3,7 @@
 rm -rf dist/
 mkdir dist/
 cp -rf __main__.py db.sqlite3 user oss objects common buckets account dist/
-pip freeze > x.txt
+pip3 freeze > x.txt
 pip3 install -r x.txt --target dist/
 rm -f x.txt
 shiv --site-packages dist --compressed -p '/usr/bin/env python3' -o oss.pyz -e oss.main
