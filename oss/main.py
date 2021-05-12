7389453fb58b08aba8b3bdf2fa6a5f192a233552
deng guibao <guibao.deng@furongcloud.com>
2021-05-12 16:31:59 +0800

    update all_in_one package entrypoint

--- oss/main.py
+++ oss/main.py
@@ -1,7 +1,8 @@
 import os
 import sys
-
 import django
+from common.verify import verify_ip_addr
+
 
 # setup django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oss.settings")
@@ -15,8 +16,31 @@ except IndexError:
 if production:
     import gunicorn.app.wsgiapp as wsgi
 
+    default_port = 8000
+    default_host = '0.0.0.0'
+
+    try:
+        host = sys.argv[2]
+    except ValueError:
+        host = default_host
+    except IndexError:
+        host = default_host
+
+    if not verify_ip_addr(host):
+        host = default_host
+
+    try:
+        port = int(sys.argv[3])
+    except ValueError:
+        port = default_port
+    except IndexError:
+        port = default_port
+
+    if 0 < port > 65536:
+        port = default_port
+
     # This is just a simple way to supply args to gunicorn
-    sys.argv = [".", "oss.wsgi", "--bind=0.0.0.0:8000", "--workers=4"]
+    sys.argv = [".", "oss.wsgi", "--bind=%s:%s" % (host, port),  "--workers=4"]
 
     wsgi.run()
 else:
